cbdcdaf8b665813bd134e7dfb20d74702f86ab73
Pavan <pavannarindi@gmail.com>
2026-03-28 23:09:43 +0530

    Removed Twilio secret

--- server/server.js
+++ server/server.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const twilio = require("twilio");
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // 🔴 PUT YOUR REAL TWILIO DETAILS HERE
-const client = twilio("AC0a85e32951dd979aaecad969673b49eb", "495af9fb8bff80480a78d70a754a65dc");
-
 
 app.post("/send-sms", async (req, res) => {
   const { phone, message } = req.body;
